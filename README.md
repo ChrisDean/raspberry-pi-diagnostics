f6ec43cfd4bd8354702b30e668d0fe66ee7b0fca
ChrisDean <chris@digitaltactics.co.uk>
2023-08-15 13:56:03 +0100

    add install as service script

--- README.md
+++ README.md
@@ -9,7 +9,25 @@ It should just run and work. If it doesn't do feel free to submit a bug report a
 a PR for your fix.
 
 Usage:
-clone this repo and run
+clone this repo and: 
+
+#1 Run the following to install necessary tools and libraries
 ```
-python ./run.py
+python ./dependencies_check.py
 ```
+
+#2 Run the following to perform the diagnostics
+```
+python ./diagnostics.py
+```
+
+#3 Optional: Run the following to install the diagnotics script as a service
+so it runs on boot (good for plugin, test board, and go)
+```
+python ./install_as_service.py
+```
+
+## Limitations
+The latest raspbian removes access to vcgencmd which has meant workarounds to attempt to aquire similar information 
+to that which vcgencmd returned originally - the tests here are fine for my use case but you may want to exand on them.
+If you do, please submit a PR!
---
+++ install_as_service.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+
+SERVICE_NAME = "run_diagnostics_after_desktop.service"
+SERVICE_PATH = f"/etc/systemd/system/{SERVICE_NAME}"
+SCRIPT_PATH = "/usr/local/bin/run_in_terminal.sh"
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+MAIN_SCRIPT_PATH = os.path.join(CURRENT_DIR, "diagnostics.py")
+
+
+def create_service_script():
+    with open(SCRIPT_PATH, 'w') as f:
+        f.write("#!/bin/bash\n\n")
+        f.write(f"xterm -e \"{MAIN_SCRIPT_PATH}\"")
+
+    os.chmod(SCRIPT_PATH, 0o755)
+
+
+def create_service():
+    service_content = f"""[Unit]
+Description=Run diagnostics script in terminal after desktop loads
+After=graphical.target
+
+[Service]
+User=pi
+Environment=DISPLAY=:0
+ExecStart={SCRIPT_PATH}
+
+[Install]
+WantedBy=graphical.target
+"""
+
+    with open(SERVICE_PATH, 'w') as f:
+        f.write(service_content)
+
+    subprocess.run(["sudo", "systemctl", "daemon-reload"])
+    subprocess.run(["sudo", "systemctl", "enable", SERVICE_NAME])
+
+
+def remove_service():
+    if os.path.exists(SERVICE_PATH):
+        subprocess.run(["sudo", "systemctl", "disable", SERVICE_NAME])
+        os.remove(SERVICE_PATH)
+
+    if os.path.exists(SCRIPT_PATH):
+        os.remove(SCRIPT_PATH)
+
+    subprocess.run(["sudo", "systemctl", "daemon-reload"])
+
+
+def main():
+    choice = input("Do you want to setup (s) or remove (r) the service? [s/r]: ").strip().lower()
+
+    if choice == 's':
+        create_service_script()
+        create_service()
+        print(f"Service {SERVICE_NAME} has been set up!")
+    elif choice == 'r':
+        remove_service()
+        print(f"Service {SERVICE_NAME} has been removed!")
+    else:
+        print("Invalid choice!")
+
+
+if __name__ == "__main__":
+    main()
